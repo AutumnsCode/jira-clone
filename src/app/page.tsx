3c1e8c06f3b3ca5742d1f026f92251330ebd7b5b
AutumnCod <109141163+AutumnsCode@users.noreply.github.com>
2024-10-12 19:24:12 +0200

    trying to sign n

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import { Button } from '@/components/ui/button';
+import { getCurrent } from "@/features/auth/actions";
+import { UserButton } from "@/features/auth/components/user-button";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+	const user = await getCurrent();
+	
+	if (!user) redirect('/sign-in');
 	return (
 		<div>
-			<Button variant='teritary' disabled>Click me</Button>
-			<p className='font-bold text-red-600'>Help</p>
+			<UserButton />
 		</div>
 	);
 }
--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,6 +1,5 @@
 import 'server-only';
 import { Client, Account } from 'node-appwrite';
-
 export async function createAdminClient() {
 	const client = new Client()
 		.setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
